47687fec357ca2bdff76e4b0289dac9c57ccfdf9
Pierre Wieser <pwieser@trychlos.org>
2023-01-27 16:49:00 +0100

    Update package definition to publish to atmospherejs.com

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'pwix:tolert',
-    version: '1.0.0',
-    summary: 'A Bootstrap-based Meteor package which provides toast-alert combination to display auto-remove messages',
+    version: '1.0.1',
+    summary: 'A Bootstrap-based Meteor package which provides toast-alert combination as auto-remove messages',
     git: 'https://github.com/trychlos/pwix-tolert',
     documentation: 'README.md'
 });
@@ -17,15 +17,15 @@ Package.onUse( function( api ){
 Package.onTest( function( api ){
     configure( api );
     api.use( 'tinytest' );
-    api.use( 'pwi:tolert' );
+    api.use( 'pwix:tolert' );
     api.mainModule( 'test/js/index.js' );
 });
 
 function configure( api ){
     api.versionsFrom( '1.8.1' );
-    api.use( 'blaze-html-templates', 'client' );
+    api.use( 'blaze-html-templates@2.0.0', 'client' );
     api.use( 'ecmascript', 'client' );
-    api.use( 'less', 'client' );
+    api.use( 'less@4.0.0', 'client' );
     api.use( 'mongo', 'client' );
     api.addFiles( 'src/client/components/tlMessage/tlMessage.js', 'client' );
 }
